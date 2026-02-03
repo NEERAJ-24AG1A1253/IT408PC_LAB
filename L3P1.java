5d01f72e69620bd4c88130824480e5e527015098
NEERAJ-24AG1A1253 <neeraj24ag1a1253@gmail.com>
2026-02-03 14:54:36 +0530

    Add files via upload

---
+++ L3P1.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class L3P1{
+	static int view = 9;
+	public static void display(){
+	System.out.println(view);
+	}
+	public int show(){
+	return (view);
+	}
+		public static void main(String[]args){
+		}
+}
+		
+		
