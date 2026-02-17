78141a657a084d11a090268a3be27b3a5ac87e83
NEERAJ-24AG1A1253 <neeraj24ag1a1253@gmail.com>
2026-02-17 16:08:20 +0530

    Update L5P4.java

--- LAB 5/L5P4.java
+++ LAB 5/L5P4.java
@@ -1,5 +1,11 @@
 import java.lang.reflect.Method;
 import java.lang.annotation.*;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Bullet{
+	String str();
+	double dbl();
+}
 class L5P4{
 	@Bullet(str="Camlin",dbl=12.36658)
 	public static void anyMethod(){
@@ -18,4 +24,5 @@ class L5P4{
 		anyMethod();
 	}
 }
+
 		
