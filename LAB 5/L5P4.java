88fdfa293ee33edaf554cecc38f218fe5b4900a1
NEERAJ-24AG1A1253 <neeraj24ag1a1253@gmail.com>
2026-02-17 16:07:48 +0530

    Add files via upload

---
+++ LAB 5/L5P4.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Method;
+import java.lang.annotation.*;
+class L5P4{
+	@Bullet(str="Camlin",dbl=12.36658)
+	public static void anyMethod(){
+		try{
+			Method mtd = L5P3.class.getMethod("anyMethod");
+			Bullet b = mtd.getAnnotation(Bullet.class);
+			System.out.println("String...."+b.str());
+			System.out.println("Double...."+b.dbl());
+		}catch (NoSuchMethodException e){
+			System.out.println("Method not found:"+e.getMessage());
+		}catch(Exception e){
+			System.out.println("general exception:"+e.getMessage());
+		}
+		}
+	public static void main(String...args){
+		anyMethod();
+	}
+}
+		
